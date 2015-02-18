231cc7f65dddeeabb323131ecc2d0613ef63ae56
taylsonmartinez <taylson.martinez@gmail.com>
2015-02-18 13:32:32 -0200

    pt-br

--- application/controllers/produtos.php
+++ application/controllers/produtos.php
@@ -22,7 +22,7 @@ class Produtos extends CI_Controller {
 
     public function novo(){
         $this->load->library("form_validation");
-        $this->form_validation->set_rules("nome", "nome", "trim|required|min_length[5]");
+        $this->form_validation->set_rules("nome", "nome", "required|min_length[5]|max_length[100]|callback_nao_tenha_a_palavra_melhor");
         $this->form_validation->set_rules("descricao", "descricao",  "trim|required|min_length[10]");
         $this->form_validation->set_rules("preco", "preco",  "trim|required");
         $this->form_validation->set_error_delimiters("<p class='alert alert-danger', </p>");
@@ -56,5 +56,16 @@ class Produtos extends CI_Controller {
         $this->load->helper("typography");
         $dados = array("produto"=>$produto);
         $this->load->view("produtos/mostra", $dados);
+    }
+
+     public function nao_tenha_a_palavra_melhor($nome) {
+        $posicao = strpos($nome, "Melhor");
+        if($posicao === FALSE) { 
+            return TRUE; 
+        } else {
+            $this->form_validation->set_message("nao_tenha_a_palavra_melhor", "O campo '%s' não pode conter a palavra 'melhor'");
+            return FALSE;
+        }
+
     }
 } 
