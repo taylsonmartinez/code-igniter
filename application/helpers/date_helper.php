c3a78b7e1f619ff9450bd0f196a93d8c373de1ec
taylsonmartinez <taylson.martinez@gmail.com>
2015-02-18 16:31:52 -0200

    vendas dos produtos

---
+++ application/helpers/date_helper.php
@@ -0,0 +1,5 @@
+<?php
+function dataPtBrParaMysql($dataPtBr) {
+    $partes = explode("/", $dataPtBr);
+    return "{$partes[2]}-{$partes[1]}-{$partes[0]}";
+}
---
+++ application/models/vendas_model.php
@@ -0,0 +1,7 @@
+<?php
+class Vendas_model extends CI_Model {
+	
+    public function salva($venda) {
+        $this->db->insert("vendas", $venda);
+    }
+}
